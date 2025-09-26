99981fc6798f21993339ba1f4247472ec2793856
RJalol <109087737+RJalol@users.noreply.github.com>
2025-09-26 19:20:21 +0500

    Add files via upload

---
+++ lesson-2/homework/add_two_numbers.py
@@ -0,0 +1,84 @@
+# x=5
+# y=6.5
+# # c='salom'
+# # z=x+y
+# # print(z)
+# # print(c)
+# # # print("type of x",type(x))
+# # # print("type of y",type(y))
+# # # print("type of z",type(z))
+# # # print("type of c",type(c))
+# # #Numeric Types --
+# # # comment ctrl+/
+# # int
+# # x=6
+# # y=6.7
+# # print("type of y",type(y))
+
+# # y=int(5.9)
+# # print("y=",y)
+
+# # print("type of y",type(y))
+
+# #result:5 
+# float
+# s=6.764
+# s=float(45)
+# print("s=",s)
+# print("type of s",type(s))
+
+# # complex
+# # decimal
+
+# # fractions
+
+
+# # Numeric Operations
+# # *,/,-,+
+# # // floor division
+# # % Remainder
+# # 7%3=? 2*3+1
+# # 1560000
+# # 1 mln
+# # 5 sto K
+# # 6 desyat K
+# divmod(5,2)#5/2 2 selix 1 osatatoq
+# # // floor division
+# # % Remainder
+# abs(-56)#56
+# pow(5,2)# 5^2
+# 5**2 #5^2
+# k=6
+# K=7
+# print('k=',k)
+# print('K=',K)
+
+# # Rules for Python variables:
+
+# # A variable name must start with a letter or the underscore character
+# # A variable name cannot start with a number
+# # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# # Variable names are case-sensitive (age, Age and AGE are three different variables)
+# # Augmented Assignment
+# # +=, -=, *=, /=, %=, **=
+# x=2
+# print('x=',x)
+# # x=x+1
+# x+=1
+# print('x=',x)
+# print(x,y,"check")
+
+
+l=int(input('enter paramentr l='))
+print("type l",type(l))
+
+print("l=",l)
+# verion 1
+l=int(input('enter paramentr l='))+20
+
+print("type l",type(l))
+# verion 2
+
+print("l=",l+20)
+
+print(4**3)
